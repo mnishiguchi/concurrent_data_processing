bb68534c81f87c1d024b9b99a82e3b4ffd300921
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2021-02-14 12:57:00 -0500

    Add basic code

--- lib/sender.ex
+++ lib/sender.ex
@@ -1,18 +1,23 @@
 defmodule Sender do
   @moduledoc """
   Documentation for `Sender`.
-  """
-
-  @doc """
-  Hello world.
 
   ## Examples
 
-      iex> Sender.hello()
-      :world
+      # Send one email
+      emails |> Enum.at(0) |> Sender.send_one_email()
 
+      # Send many emails
+      Sender.send_many_emails(emails)
   """
-  def hello do
-    :world
+
+  def send_one_email(email) do
+    Process.sleep(3000)
+    IO.puts("Email to #{email} sent")
+    {:ok, "email_sent"}
+  end
+
+  def send_many_emails(emails) do
+    Enum.each(emails, &send_one_email/1)
   end
 end
--- test/sender_test.exs
+++ test/sender_test.exs
@@ -2,7 +2,7 @@ defmodule SenderTest do
   use ExUnit.Case
   doctest Sender
 
-  test "greets the world" do
-    assert Sender.hello() == :world
-  end
+  # test "greets the world" do
+  #   assert Sender.hello() == :world
+  # end
 end
